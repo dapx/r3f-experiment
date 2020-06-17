c07c2e21960182425d132c874271ab3cb192c140
Daniel André da Silva <dap1995@gmail.com>
2020-06-17 00:26:49 -0300

    Create canvas with components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import { Canvas } from 'react-three-fiber';
+import { Physics } from 'use-cannon'
+import Plane from './components/Plane'
+import Box from './components/Box'
+import StaticBox from './components/StaticBox'
+import Controls from './components/Controls'
+import Player from './components/Player'
+import Sun from './components/Sun';
 import './App.css';
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+function getRandomPositions() {
+  return [getRandomInt(1, 10), getRandomInt(10, 100), 0]
+}
+
 function App() {
+  const ref = useRef()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas
+      style={{ background: "#171717" }}
+      camera={{fov: 100, position: [0, 5, 0]}}
+      shadowMap
+      colorManagement
+    >
+      <Physics>
+        <ambientLight />
+        <Sun />
+        <fog attach="fog" args={['#000', 0, 50]} />
+        { [...Array(100).keys()].map((index) => <Box key={`box-${index}`} position={getRandomPositions()} />) }
+        { [...Array(100).keys()].map((index) => <StaticBox key={`box-${index}`} position={getRandomPositions()} />) }
+        <Plane />
+        <Player ref={ref} />
+      </Physics>
+      <Controls playerRef={ref} />
+    </Canvas>
   );
 }
 
